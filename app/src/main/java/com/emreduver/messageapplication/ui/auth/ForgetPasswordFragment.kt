06e3779a6e0de8b50c3922ccb40fc2933ccf26c6
Emre Düver <dver.emre@gmail.com>
2021-08-30 18:28:11 +0300

    ForgetPassword eklendi

--- app/src/main/java/com/emreduver/messageapplication/ui/auth/ForgetPasswordFragment.kt
+++ app/src/main/java/com/emreduver/messageapplication/ui/auth/ForgetPasswordFragment.kt
@@ -1,5 +1,6 @@
 package com.emreduver.messageapplication.ui.auth
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,7 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.emreduver.messageapplication.R
+import com.emreduver.messageapplication.entities.send.auth.Login
+import com.emreduver.messageapplication.ui.main.UserActivity
+import com.emreduver.messageapplication.utilities.HelperService
 import com.emreduver.messageapplication.viewmodels.auth.ForgetPasswordViewModel
+import com.emreduver.messageapplication.viewmodels.auth.LoginViewModel
+import kotlinx.android.synthetic.main.forget_password_fragment.*
+import kotlinx.android.synthetic.main.login_fragment.*
 
 class ForgetPasswordFragment : Fragment() {
 
@@ -27,6 +34,31 @@ class ForgetPasswordFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ForgetPasswordViewModel::class.java)
+
+        btnForgetPassword.setOnClickListener{
+            forgetPassword()
+        }
+    }
+
+    private fun forgetPassword(){
+        if (!editTextForForgetPassword.editText?.text.toString().isNullOrEmpty()){
+            val email = editTextForForgetPassword.editText!!.text.toString()
+            viewModel.forgetPassword(email).observe(viewLifecycleOwner){
+                if (it){
+                    HelperService.showMessageByToast("Şifrenizi sıfırlamak için e-posta adresinize göderilen maili açınız.")
+                }else{
+                    errorListener()
+                }
+            }
+        }else{
+            HelperService.showMessageByToast("Lütfen e-posta adresinizi giriniz!")
+        }
+    }
+
+    private fun errorListener() {
+        viewModel.errorState.observe(viewLifecycleOwner, {
+            HelperService.showMessageByToast(it)
+        })
     }
 
 }
--- app/src/main/java/com/emreduver/messageapplication/viewmodels/auth/ForgetPasswordViewModel.kt
+++ app/src/main/java/com/emreduver/messageapplication/viewmodels/auth/ForgetPasswordViewModel.kt
@@ -1,6 +1,29 @@
 package com.emreduver.messageapplication.viewmodels.auth
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.emreduver.messageapplication.services.api.UserService
+import com.emreduver.messageapplication.utilities.IViewModelState
+import com.emreduver.messageapplication.utilities.LoadingState
+import kotlinx.coroutines.launch
 
-class ForgetPasswordViewModel : ViewModel() {
+class ForgetPasswordViewModel : ViewModel(), IViewModelState {
+    override var loadingState: MutableLiveData<LoadingState> = MutableLiveData<LoadingState>()
+    override var errorState: MutableLiveData<String> = MutableLiveData<String>()
+
+    fun forgetPassword(email:String): LiveData<Boolean> {
+        loadingState.value = LoadingState.Loading
+        val status = MutableLiveData<Boolean>()
+
+        viewModelScope.launch {
+            val response = UserService.resetPasswordRequest(email)
+            status.value = response.Success
+            loadingState.value = LoadingState.Loaded
+            if (!response.Success)
+                errorState.value = response.Message!!
+        }
+        return status
+    }
 }
